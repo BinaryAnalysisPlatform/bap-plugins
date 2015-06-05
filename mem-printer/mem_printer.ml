510770508f336a197122febbe76eea2c411571c7
Rijnard <rvantonder@gmail.com>
2015-06-05 16:10:49 +0000

    that was silly

--- mem-printer/mem_printer.ml
+++ mem-printer/mem_printer.ml
@@ -4,7 +4,6 @@ open Format
 
 let main project =
   Project.memory project |> Memmap.to_sequence |> Seq.iter ~f:(fun (mem,x) ->
-      printf "%s(%a)@.%a@." (Value.tagname x) Value.pp x Memory.pp mem);
-  ()
+      printf "%s(%a)@.%a@." (Value.tagname x) Value.pp x Memory.pp mem)
 
 let () = Project.register_pass' "mem_printer" main
