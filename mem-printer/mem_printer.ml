c42b187f910228aaf5ac41b7569b42e9a2bca13f
Rijnard <rvantonder@gmail.com>
2015-06-05 16:10:05 +0000

    don't need args

--- mem-printer/mem_printer.ml
+++ mem-printer/mem_printer.ml
@@ -2,9 +2,9 @@ open Core_kernel.Std
 open Bap.Std
 open Format
 
-let main args project =
+let main project =
   Project.memory project |> Memmap.to_sequence |> Seq.iter ~f:(fun (mem,x) ->
-    printf "%s(%a)@.%a@." (Value.tagname x) Value.pp x Memory.pp mem);
-  project
+      printf "%s(%a)@.%a@." (Value.tagname x) Value.pp x Memory.pp mem);
+  ()
 
-let () = Project.register_pass_with_args "mem_printer" main
+let () = Project.register_pass' "mem_printer" main
