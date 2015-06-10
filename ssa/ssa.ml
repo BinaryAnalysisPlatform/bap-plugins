26480b6d960b566cc36548d20341afdbf2b2013d
Ivan Gotovchits <ivg@ieee.org>
2015-06-10 18:46:42 -0400

    switched to Appel algorithm

--- ssa/ssa.ml
+++ ssa/ssa.ml
@@ -2,49 +2,6 @@ open Core_kernel.Std
 open Bap.Std
 open Format
 
-let transfer term_type blk adder bld =
-  Term.to_sequence term_type blk |> Seq.iter ~f:(adder bld)
-
-let builder_of_blk
-    ?(same_tid=true)
-    ?(copy_phis=false) ?(copy_defs=false) ?(copy_jmps=false) blk =
-  let tid = if same_tid then Term.tid blk else Tid.create () in
-  let b = Blk.Builder.create ~tid ()
-      ~phis:(Term.length phi_t blk)
-      ~defs:(Term.length def_t blk)
-      ~jmps:(Term.length jmp_t blk) in
-  if copy_phis then transfer phi_t blk Blk.Builder.add_phi b;
-  if copy_defs then transfer def_t blk Blk.Builder.add_def b;
-  if copy_jmps then transfer jmp_t blk Blk.Builder.add_jmp b;
-  b
-
-let substitute vars = (object
-  inherit Bil.mapper as super
-  method! map_sym z =
-    match Map.find vars z with
-    | Some v -> v
-    | None -> super#map_sym z
-end)#map_exp
-
-let rename v = Var.(create ~tmp:true (name v) (typ v))
-
-let ssa_blk blk =
-  let bld = builder_of_blk ~copy_phis:true ~copy_jmps:true blk in
-  Term.to_sequence def_t blk |>
-  Seq.fold ~init:Var.Map.empty ~f:(fun subs def ->
-      let v,e = Def.(lhs def, rhs def) in
-      let v' = rename v in
-      let e = substitute subs e in
-      let def = Def.with_rhs (Def.with_lhs def v') e in
-      Blk.Builder.add_def bld def;
-      Map.add subs ~key:v ~data:v') |> fun subs ->
-  Blk.Builder.result bld |>
-  Term.map jmp_t ~f:(fun jmp ->
-      Jmp.cond jmp |> substitute subs |> Jmp.with_cond jmp) |>
-  Term.filter phi_t ~f:(fun phi ->
-      Seq.length_is_bounded_by ~min:2 (Phi.defs phi))
-
-
 let succ_of_jmp  jmp = match Jmp.kind jmp with
   | Goto (Direct tid) -> Some tid
   | Call t -> Option.(Call.return t >>= function
@@ -55,49 +12,69 @@ let succ_of_jmp  jmp = match Jmp.kind jmp with
 
 (** builds a map from a tid to all blocks that have jumps that lead
     this tid. *)
-let build_rdep sub : blk term list Tid.Map.t =
+let build_rdep sub : tid list Tid.Map.t =
   Term.to_sequence blk_t sub |>
   Seq.fold ~init:Tid.Map.empty ~f:(fun ins blk ->
       Term.to_sequence jmp_t blk |>
       Seq.fold ~init:ins ~f:(fun ins jmp ->
           Option.value_map (succ_of_jmp jmp) ~default:ins ~f:(fun tid ->
-              Map.add_multi ins ~key:tid ~data:blk)))
+              Map.add_multi ins ~key:tid ~data:(Term.tid blk))))
 
-let dominance_frontier sub entry : blk term -> blk term list =
+type dom = {
+  frontier : tid -> tid list;
+  children : tid -> tid list;
+}
+
+let create_dom rdep sub entry =
   let module Dom = Graph.Dominator.Make(struct
-      type t = (sub term * blk term list Tid.Map.t)
-      module V = Blk
-      let pred (_,rdep) blk = match Map.find rdep (Term.tid blk) with
+      type t = (sub term * tid list Tid.Map.t)
+      module V = Tid
+      let pred (_,rdep) tid = match Map.find rdep tid with
         | None -> []
         | Some xs -> xs
-      let succ (sub,_) blk =
-        Term.to_sequence ~rev:true jmp_t blk |>
-        Seq.filter_map ~f:(fun jmp -> match succ_of_jmp jmp with
-            | Some t -> Term.find blk_t sub t
-            | None -> None) |>
-        Seq.to_list_rev
+      let succ (sub,_) tid = match Term.find blk_t sub tid with
+        | None -> []
+        | Some blk ->
+          Term.to_sequence jmp_t ~rev:true blk |>
+          Seq.filter_map ~f:succ_of_jmp |>
+          Seq.to_list_rev
       let fold_vertex f (sub,_) init =
         Term.to_sequence blk_t sub |>
-        Seq.fold ~init ~f:(fun a v -> f v a)
+        Seq.fold ~init ~f:(fun a v -> f (Term.tid v) a)
       let iter_vertex f (sub,_) =
-        Term.to_sequence blk_t sub |> Seq.iter ~f
+        Term.to_sequence blk_t sub |>
+        Seq.map ~f:Term.tid |> Seq.iter ~f
       let nb_vertex (sub,_) = Term.length blk_t sub
     end) in
-  let cfg = sub, build_rdep sub in
-  let idom = Dom.compute_idom cfg entry in
+  let cfg = sub, rdep in
+  let idom = Dom.compute_idom cfg (Term.tid entry) in
   let dom_tree = Dom.idom_to_dom_tree cfg idom in
-  Dom.compute_dom_frontier cfg dom_tree idom
+  {
+    frontier = Dom.compute_dom_frontier cfg dom_tree idom;
+    children = dom_tree
+  }
 
+
+(** [iterated_frontier frontier bs] given a [frontier] function, that
+    for a each block [b] returns its dominance frontier, compute an
+    iterated dominance frontier of a set of block [bs]. Iterated
+    dominance frontier is defined inductively as
+    [IDF_1(S) = DF(S); IDF_n(S) = DF(S U IDF_{n-1}(S))],
+    where [DF(S)] computes a union of dominance frontiers of each
+    block in [S].  The function returns a result of [IDF_k], where
+    [k] is a fixpoint, i.e., such value that [IDF_k = IDF_{k-1}].  See
+    section 8.11 of Advanced Compiler Design and Implementation
+    [ISBN-10: 1558603204].*)
 let iterated_frontier frontier blks =
-  let df_set = Set.fold ~init:Blk.Set.empty ~f:(fun dfs b ->
+  let df = Set.fold ~init:Tid.Set.empty ~f:(fun dfs b ->
       List.fold (frontier b) ~init:dfs ~f:Set.add) in
-  let blks = List.fold blks ~init:Blk.Set.empty ~f:Set.add in
-  let rec loop idf =
-    let idf' = df_set (Set.union idf blks) in
-    if Set.equal idf idf' then idf'
-    else loop idf' in
-  loop Blk.Set.empty
+  let blks = List.fold blks ~init:Tid.Set.empty ~f:Set.add in
+  let rec fixpoint idf =
+    let idf' = df (Set.union idf blks) in
+    if Set.equal idf idf' then idf' else fixpoint idf' in
+  fixpoint Tid.Set.empty
 
+(* todo, collect only globals *)
 let collect_vars sub =
   Term.to_sequence blk_t sub |>
   Seq.fold ~init:Var.Set.empty ~f:(fun vars blk ->
@@ -107,45 +84,105 @@ let collect_vars sub =
 
 let blocks_that_define_var var sub =
   Term.to_sequence blk_t sub |>
-  Seq.filter_map ~f:(fun blk ->
+  Seq.filter ~f:(fun blk ->
       Term.to_sequence ~rev:true def_t blk |>
-      Seq.find_map ~f:(fun def ->
-          Option.some_if Var.(Def.lhs def = var) (blk,def))) |>
+      Seq.exists ~f:(fun def -> Var.(Def.lhs def = var))) |>
+  Seq.map ~f:Term.tid |>
   Seq.to_list_rev
 
-let insert_phi x defs user =
-  let put_defs init defs = List.fold defs ~init ~f:Phi.add_def in
-  Term.to_sequence phi_t user |>
-  Seq.find ~f:(fun phi -> Phi.lhs phi = x) |> function
-  | Some phi ->
-    Term.update phi_t user @@
-    put_defs phi defs
-  | None -> match defs with
-    | [] -> user
-    | d :: ds ->
-      Term.append phi_t user @@
-      put_defs (Phi.create x d) ds
+let substitute vars = (object
+  inherit Bil.mapper as super
+  method! map_sym z =
+    match Hashtbl.find vars z with
+    | None | Some [] -> z
+    | Some (d :: _) -> d
+end)#map_exp
 
+let renumber v = Var.(create ~tmp:true (name v) (typ v))
 
-let print_ifs_of_var frontier entry bs x =
-  printf "IFS(%a)@." Var.pp x;
-  Set.iter (iterated_frontier frontier (entry :: bs))
-    ~f:(fun b -> printf "\t%a@." Tid.pp (Term.tid b))
+let rename children phis vars sub entry =
+  let vars : var list Var.Table.t = Var.Table.create () in
+  let top v = match Hashtbl.find vars v with
+    | None | Some [] -> v
+    | Some (v :: _) -> v in
+  let pop v = Hashtbl.change vars v (function
+      | Some (x::xs) -> Some xs
+      | xs -> xs) in
+  let rename_defs blk =
+    Term.map def_t blk ~f:(fun def ->
+        let lhs = renumber (Def.lhs def) in
+        Hashtbl.add_multi vars ~key:(Def.lhs def) ~data:lhs;
+        Def.rhs def |> substitute vars |>
+        Def.with_rhs (Def.with_lhs def lhs)) in
+  let rename_jmps blk =
+    Term.map jmp_t blk
+      ~f:(Jmp.map_exp ~f:(substitute vars)) in
+  let rename_phis blk =
+    Term.map phi_t blk ~f:(fun phi ->
+        let lhs = renumber (Phi.lhs phi) in
+        Hashtbl.add_multi vars ~key:(Phi.lhs phi) ~data:lhs;
+        Phi.with_lhs phi lhs) in
+  let update_phis src dst =
+    match Map.find phis (Term.tid dst) with
+    | None -> dst
+    | Some vs ->
+      let tid = Term.tid src in
+      List.fold vs ~init:dst ~f:(fun dst v ->
+          Term.to_sequence phi_t dst |>
+          Seq.find ~f:(fun phi -> Var.(Phi.lhs phi = v)) |> function
+          | None ->
+            Phi.create v tid (Bil.var (top v)) |>
+            Term.append phi_t dst
+          | Some phi ->
+            Phi.map_exp phi ~f:(substitute vars) |>
+            Term.update phi_t dst) in
+  let rec rename sub tid =
+    let blk' = match Term.find blk_t sub tid with
+      | None -> assert false
+      | Some blk' -> blk' in
+    let blk = blk' |> rename_phis |> rename_defs |> rename_jmps in
+    let sub = Term.update blk_t sub blk in
+    Term.to_sequence jmp_t blk |>
+    Seq.fold ~init:sub ~f:(fun sub jmp -> match succ_of_jmp jmp with
+        | None -> sub
+        | Some tid -> match Term.find blk_t sub tid with
+          | None -> sub
+          | Some dst ->
+            Term.update blk_t sub (update_phis blk dst)) |>
+    fun sub ->
+    List.fold (children tid) ~init:sub ~f:rename |>
+    fun sub ->
+    Term.to_sequence phi_t blk' |>
+    Seq.iter ~f:(fun phi -> pop (Phi.lhs phi));
+    Term.to_sequence def_t blk' |>
+    Seq.iter ~f:(fun def -> pop (Def.lhs def));
+    sub in
+  rename sub entry
+
+(** [find_phi_placeholders frontier sub entry vars] given a [frontier]
+    function that for a given block returns its dominance frontier, a
+    subroutine [sub] with entry block [entry] and a set of variable
+    [vars] compute for each variable [x] in [vars] a set of blocks
+    where a phi-node for [x] should be placed.  The algorithm computes
+    an iterated dominance frontier for each variable as per section
+    8.11 of Advanced Compiler Design and Implementation [ ISBN-10:
+    1558603204].*)
+let find_phi_placeholders frontier sub entry vars =
+  Set.fold vars ~init:Tid.Map.empty ~f:(fun phis x ->
+      let bs = blocks_that_define_var x sub in
+      iterated_frontier frontier (entry :: bs) |>
+      Set.fold ~init:phis ~f:(fun phis blk ->
+          Map.add_multi phis ~data:x ~key:blk))
 
 let ssa_sub sub = match Term.first blk_t sub with
   | None -> sub
   | Some entry ->
-    let frontier = dominance_frontier sub entry in
-    collect_vars sub |> Set.fold ~init:sub ~f:(fun sub x ->
-        let (bs,defs) = blocks_that_define_var x sub |>
-                        List.unzip in
-        iterated_frontier frontier (entry :: bs) |>
-        Set.fold ~init:sub ~f:(fun sub blk ->
-            match Term.find blk_t sub (Term.tid blk) with
-            | None -> sub
-            | Some blk ->
-              Term.update blk_t sub (insert_phi x defs blk)))
-    |> Term.map blk_t ~f:ssa_blk
+    let rdep = build_rdep sub in
+    let dom = create_dom rdep sub entry in
+    let vars = collect_vars sub in
+    let phis =
+      find_phi_placeholders dom.frontier sub (Term.tid entry) vars in
+    rename dom.children phis vars sub (Term.tid entry)
 
 let main' proj =
   Term.map sub_t (Project.program proj) ~f:ssa_sub |>
