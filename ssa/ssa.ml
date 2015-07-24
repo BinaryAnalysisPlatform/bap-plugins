c2832bb4678891de54130203fc01a53d65820698
Ivan Gotovchits <ivg@ieee.org>
2015-07-24 15:42:55 -0400

    tentative update of ssa to the upcoming release.

--- ssa/ssa.ml
+++ ssa/ssa.ml
@@ -16,67 +16,7 @@ open Core_kernel.Std
 open Bap.Std
 open Format
 
-(** extracts a successor's tid from a jump term  *)
-let succ_tid_of_jmp jmp : tid option = match Jmp.kind jmp with
-  | Goto (Direct tid) -> Some tid
-  | Call t -> Option.(Call.return t >>= function
-    | Direct tid -> Some tid
-    | _ -> None)
-  | Int (_,tid) -> Some tid
-  | _ -> None
-
-let succ_of_jmp sub jmp =
-  match succ_tid_of_jmp jmp with
-  | Some tid as blk when Term.find blk_t sub tid <> None -> blk
-  | _ -> None
-
-(** builds a map from a tid to all blocks that have jumps that lead
-    this tid. *)
-let build_rdep sub : tid list Tid.Map.t =
-  Term.enum blk_t sub |>
-  Seq.fold ~init:Tid.Map.empty ~f:(fun ins blk ->
-      Term.enum jmp_t blk |>
-      Seq.fold ~init:ins ~f:(fun ins jmp ->
-          Option.value_map (succ_of_jmp sub jmp) ~default:ins ~f:(fun tid ->
-              Map.add_multi ins ~key:tid ~data:(Term.tid blk))))
-
-(** Dominance relations.  *)
-type dom = {
-  frontier : tid -> tid list;    (** dominance frontier of a node *)
-  children : tid -> tid list;    (** children of a node in a dominator tree *)
-}
-
-(** computes dominator tree an dominance frontier  *)
-let create_dom rdep sub entry =
-  let module G = struct
-    type t = (sub term * tid list Tid.Map.t)
-    module V = Tid
-    let pred (_,rdep) tid = match Map.find rdep tid with
-      | None -> []
-      | Some xs -> xs
-    let succ (sub,_) tid = match Term.find blk_t sub tid with
-      | None -> []
-      | Some blk ->
-        Term.enum jmp_t ~rev:true blk |>
-        Seq.filter_map ~f:(succ_of_jmp sub) |>
-        Seq.to_list_rev
-    let fold_vertex f (sub,_) init =
-      Term.enum blk_t sub |>
-      Seq.fold ~init ~f:(fun a v -> f (Term.tid v) a)
-    let iter_vertex f (sub,_) =
-      Term.enum blk_t sub |>
-      Seq.map ~f:Term.tid |> Seq.iter ~f
-    let nb_vertex (sub,_) = Term.length blk_t sub
-  end in
-  let module Dom = Graph.Dominator.Make(G) in
-  let cfg = sub, rdep in
-  let idom = Dom.compute_idom cfg (Term.tid entry) in
-  let dom_tree = Dom.idom_to_dom_tree cfg idom in
-  {
-    frontier = Dom.compute_dom_frontier cfg dom_tree idom;
-    children = dom_tree
-  }
-
+module Cfg = Graphlib.Ir
 
 (** [iterated_frontier frontier bs] given a [frontier] function, that
     for a each block [b] returns its dominance frontier, compute an
@@ -87,40 +27,56 @@ let create_dom rdep sub entry =
     block in [S].  The function returns a result of [IDF_k], where
     [k] is a fixpoint, i.e., such value that [IDF_k = IDF_{k-1}].  See
     section 8.11 of [1].*)
-let iterated_frontier frontier blks =
-  let df = Set.fold ~init:Tid.Set.empty ~f:(fun dfs b ->
-      List.fold (frontier b) ~init:dfs ~f:Set.add) in
-  let blks = List.fold blks ~init:Tid.Set.empty ~f:Set.add in
+let iterated_frontier f blks =
+  let df = Set.fold ~init:Blk.Set.empty ~f:(fun dfs b ->
+      Seq.fold (Frontier.enum f b) ~init:dfs ~f:Set.add) in
+  let blks = List.fold blks ~init:Blk.Set.empty ~f:Set.add in
   let rec fixpoint idf =
     let idf' = df (Set.union idf blks) in
     if Set.equal idf idf' then idf' else fixpoint idf' in
-  fixpoint Tid.Set.empty
+  fixpoint Blk.Set.empty
+
+let vars_of_exp = Exp.fold ~init:Var.Set.empty (object
+    inherit [Var.Set.t] Bil.visitor
+    method! enter_var var vars = Set.add vars var
+  end)
+
+let vars_of_label = function
+  | Indirect exp -> vars_of_exp exp
+  | Direct _ -> Var.Set.empty
 
 (** [collect_vars] traverses through subroutine [sub] and collects
     variables, that are live across multiple blocks (aka globals).
     Algorithm is described in Figure 9.9 in [[3]].*)
 let collect_vars sub =
+  let (--) = Set.diff and (++) = Set.union in
   Term.enum blk_t sub |>
-  Seq.fold ~init:(Var.Set.empty) ~f:(fun vars blk ->
+  Seq.fold ~init:Var.Set.empty ~f:(fun vars blk ->
       Term.enum def_t blk |>
       Seq.fold ~init:(vars,Var.Set.empty) ~f:(fun (vars,kill) def ->
-          let vars =
-            Exp.fold ~init:vars (object
-              inherit [Var.Set.t] Bil.visitor
-              method! enter_var v vars =
-                if Set.mem kill v then vars
-                else Set.add vars v
-            end) (Def.rhs def) in
-          vars,Set.add kill (Def.lhs def)) |> fst)
+          vars ++ (vars_of_exp (Def.rhs def) -- kill),
+          Set.add kill (Def.lhs def)) |> fun (vars,kill) ->
+      Seq.fold ~init:Var.Set.empty (Term.enum jmp_t blk)
+        ~f:(fun vars jmp ->
+            vars ++ vars_of_exp (Jmp.cond jmp) ++
+            match Jmp.kind jmp with
+            | Ret dst | Goto dst -> vars_of_label dst
+            | Int (_,_) -> Var.Set.empty
+            | Call call ->
+              vars_of_label (Call.target call) ++
+              match Call.return call with
+              | None -> Var.Set.empty
+              | Some dst -> vars_of_label dst) -- kill ++ vars)
+
+
 
 (** returns a list of blocks that contains [def] terms with lhs equal
     to [var] *)
-let blocks_that_define_var var sub : tid list =
-  Term.enum blk_t sub |>
+let blocks_that_define_var var sub : blk term list =
+  Cfg.nodes sub |>
   Seq.filter ~f:(fun blk ->
       Term.enum ~rev:true def_t blk |>
       Seq.exists ~f:(fun def -> Var.(Def.lhs def = var))) |>
-  Seq.map ~f:Term.tid |>
   Seq.to_list_rev
 
 (** [substitute vars exp] take a table of stacks of variables and
@@ -144,15 +100,12 @@ end)#map_exp
     variable with [renumber] function. It has a nice side effect of
     cleary showing a first use of a variable. And works well with our
     API to resolve input/output parameters.*)
-let rename children vars sub entry =
+let rename dom vars sub entry =
   let vars : var list Var.Table.t = Var.Table.create () in
   let nums : int Var.Table.t = Var.Table.create () in
   let top v = match Hashtbl.find vars v with
     | None | Some [] -> v
     | Some (v :: _) -> v in
-  let find_blk sub id = match Term.find blk_t sub id with
-    | None -> assert false
-    | Some vs -> vs in
   let new_name x =
     Hashtbl.change nums x (function
         | None -> Some 1
@@ -188,20 +141,15 @@ let rename children vars sub entry =
     Term.enum def_t blk' |>
     Seq.iter ~f:(fun def -> pop (Def.lhs def)) in
 
-  let rec rename_block sub tid =
-    let blk' = find_blk sub tid in
+  let rec rename_block sub blk' =
     let blk = blk' |> rename_phis |> rename_defs |> rename_jmps in
-    let sub = Term.update blk_t sub blk in
+    let sub = Cfg.Node.update blk sub in
     let sub =
-      Term.enum jmp_t blk |>
-      Seq.fold ~init:sub ~f:(fun sub jmp -> match succ_of_jmp sub jmp with
-          | None -> sub
-          | Some tid -> match Term.find blk_t sub tid with
-            | None -> sub
-            | Some dst ->
-              Term.update blk_t sub (update_phis blk dst)) in
-    let children = Tid.Set.of_list (children tid) in
-    let sub = Set.fold children ~init:sub ~f:rename_block in
+      Cfg.Node.succs blk sub |> Seq.fold ~init:sub ~f:(fun sub dst ->
+          Cfg.Node.update (update_phis blk dst) sub) in
+    let children = Cfg.nodes sub |>
+                   Seq.filter ~f:(Tree.is_child_of ~parent:blk dom) in
+    let sub = Seq.fold children ~init:sub ~f:rename_block in
     pop_defs blk;
     sub in
   rename_block sub entry
@@ -212,8 +160,8 @@ let has_phi_for_var blk x =
 (** [insert_phi_node ins blk x]   *)
 let insert_phi_node ins blk x =
   if has_phi_for_var blk x then blk
-  else List.map ins ~f:(fun tid -> tid, Bil.var x) |>
-       Phi.of_list x |>
+  else Seq.map ins ~f:(fun blk -> Term.tid blk, Bil.var x) |>
+       Seq.to_list_rev |> Phi.of_list x |>
        Term.append phi_t blk
 
 (** [insert_phi_nodes frontier sub entry vars] given a [frontier]
@@ -223,28 +171,27 @@ let insert_phi_node ins blk x =
     variable [x] in [vars] in each block that needs it. The
     algorithm computes an iterated dominance frontier for each
     variable as per section 8.11 of [1].*)
-let insert_phi_nodes frontier rdep sub entry vars =
+let insert_phi_nodes frontier sub entry vars =
   Set.fold vars ~init:sub ~f:(fun sub x ->
       let bs = blocks_that_define_var x sub in
       iterated_frontier frontier (entry :: bs) |>
       Set.fold ~init:sub ~f:(fun sub blk ->
-          match Map.find rdep blk with
-          | None | Some [] | Some [_] -> sub
-          | Some ins ->
-            let blk = Option.value_exn (Term.find blk_t sub blk) in
-            Term.update blk_t sub (insert_phi_node ins blk x)))
+          let blk = Term.find_exn blk_t (Cfg.to_sub sub) (Term.tid blk) in
+          let ins = Cfg.Node.preds blk sub in
+          Cfg.Node.update (insert_phi_node ins blk x) sub))
 
 (** transforms subroutine into a semi-pruned SSA form.  *)
 let ssa_sub sub =
   match Term.first blk_t sub with
   | None -> sub
   | Some entry ->
-    let rdep = build_rdep sub in
-    let dom = create_dom rdep sub entry in
     let vars = collect_vars sub in
-    let sub =
-      insert_phi_nodes dom.frontier rdep sub (Term.tid entry) vars in
-    rename dom.children vars sub (Term.tid entry)
+    let cfg = Cfg.of_sub sub in
+    let dom = Graphlib.dominators (module Cfg) cfg entry in
+    let dom_frontier = Graphlib.dom_frontier (module Cfg) cfg dom in
+    let sub = insert_phi_nodes dom_frontier cfg entry vars in
+    rename dom vars sub entry |>
+    Cfg.to_sub
 
 let ssa_program = Term.map sub_t ~f:ssa_sub
 
