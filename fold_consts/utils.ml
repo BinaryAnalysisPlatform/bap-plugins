96922a969b098af2c55b5f58fefec9160903ebf4
Ivan Gotovchits <ivg@ieee.org>
2015-08-19 12:30:15 -0400

    lost file

---
+++ fold_consts/utils.ml
@@ -0,0 +1,8 @@
+open Core_kernel.Std
+open Bap.Std
+
+let mem_elt_size arch =
+  let module Target = (val target_of_arch arch) in
+  match Var.typ Target.CPU.mem with
+  | Type.Imm _ -> assert false
+  | Type.Mem (_,s) -> s
