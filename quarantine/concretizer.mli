08299fce4b240684c6c77ee4efe03f6e066a7152
Ivan Gotovchits <ivg@ieee.org>
2015-10-21 15:56:43 -0400

    implemented concretizer
    and started too fill up the bag of tricks.

--- quarantine/concretizer.mli
+++ quarantine/concretizer.mli
@@ -1,4 +1,19 @@
 open Core_kernel.Std
 open Bap.Std
 
-class ['a] constant : word -> ['a] expi
+type summary = (var * Bil.value) list
+
+
+class ['a] t :
+  ?summary:(call -> summary option) ->
+  ?mapping:(addr -> tid option) ->
+  ?const:word -> unit -> ['a] biri
+
+
+(** a bag of tricks  *)
+module Tricks : sig
+  class ['a] constant : word -> ['a] expi
+  class ['a] indirects_from_mapping : (word -> tid option) -> ['a] biri
+  class ['a] shortcut_indirect_calls : ['a] biri
+  class ['a] summarize_calls : (call -> summary option) -> ['a] biri
+end
