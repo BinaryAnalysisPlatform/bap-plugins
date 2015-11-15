3769312494a1d4c3db8be392861fc4d0e5ea2452
Ivan Gotovchits <ivg@ieee.org>
2015-11-15 13:50:58 -0500

    solver: implemented positional seeding
    Now calls are seeded based on positional arguments.
    The seeding takes into account argument type and direction.

--- quarantine/seeder.ml
+++ quarantine/seeder.ml
@@ -24,8 +24,7 @@ let arg_matches intent v cs arg =
         Set.mem free var
       | _ -> false)
 
-let def_of_arg intent v cs arg =
-  require (intent_matches arg intent) >>= fun () ->
+let def_of_arg arg =
   let x = Bil.var (Arg.lhs arg) in
   match Arg.rhs arg with
   | Bil.Var var -> Some (Def.create var x)
@@ -33,46 +32,64 @@ let def_of_arg intent v cs arg =
     Some (Def.create m (Bil.store ~mem ~addr:a x e s))
   | _ -> None
 
-(* TODO: this should be reimplemented, to follow
-   this algorithm:
+let use_of_arg arg =
+  let x = Arg.lhs arg in
+  let e = Arg.rhs arg in
+  Some (Def.create x e)
 
-   1. foreach variable
-   1.1 find argument with matching position
-   1.2 do not forget to check other constraints
-       like predicates and vars
-   1.2 create argument
-   1.3 ....
-   1.4 PROFIT
+let sat_pred constr term v var =
+  List.for_all constr ~f:(function
+      | Constr.Fun (id,v') ->
+        V.(v = v') ==> Predicate.test id term var
+      | _ -> true)
 
-   see you in Sunday :(
-*)
-let defs_of_args intent v cs args =
-  Seq.filter_map args ~f:(def_of_arg intent v cs)
+let exists_sat_pred constr term v vars =
+  Set.exists vars ~f:(fun var -> sat_pred constr term v var)
 
-let tag_arg_def jmp call term =
-  seed_with (Term.tid jmp) term
+let arg_free_vars arg = Arg.rhs arg |> Exp.free_vars
 
-let already_seeded blk var =
-  Term.enum def_t blk |> Seq.exists ~f:(fun def ->
-      Set.mem (Def.free_vars def) var &&
-      Term.has_attr def Taint.reg)
-
-let prepend_def blk def =
-  if already_seeded blk (Def.lhs def)
-  then blk
-  else Term.prepend def_t blk def
-
-let append_def blk def =
-  (* TODO: prepend only if not seeded already *)
-  Term.append def_t blk def
-
-
-let sat_pred constr term v vars =
+let sat_constrs constr arg v =
+  let vars = arg_free_vars arg in
   Set.exists vars ~f:(fun var ->
       List.for_all constr ~f:(function
           | Constr.Fun (id,v') ->
-            V.(v = v') ==> Predicate.test id term var
-          | _ -> true))
+            V.(v = v') ==> Predicate.test id arg var
+          | Constr.Var (v',var') ->
+            V.(v = v') ==> Var.(var = var')
+          | Constr.Dep _ -> true))
+
+let tag_by_sort s def =
+  let taint_t = if s = S.Ptr then Taint.ptr else Taint.reg in
+  Term.set_attr def taint_t
+
+let defs_of_args intent vs defn args =
+  let args = Seq.to_array args in
+  let cons = Defn.constrs defn in
+  let sort = List.Assoc.find ~equal:V.equal (Defn.vars defn) in
+  let make_def = if intent = In then use_of_arg else def_of_arg in
+  let nth_arg i = try Some (Array.nget args i) with _ -> None in
+  List.filter_mapi vs ~f:(fun n v ->
+      nth_arg (n - 1) >>= fun arg ->
+      require (v > 0) >>= fun () ->
+      require (intent_matches arg intent) >>= fun () ->
+      require (sat_constrs cons arg v) >>= fun () ->
+      sort v >>= fun s ->
+      make_def arg >>|
+      tag_by_sort s)
+
+
+let already_seeded blk var =
+  Term.enum def_t blk |> Seq.exists ~f:(fun def ->
+      Var.equal (Def.lhs def) var &&
+      (Term.has_attr def Taint.reg ||
+       Term.has_attr def Taint.ptr))
+
+let add_def intent blk def =
+  if already_seeded blk (Def.lhs def)
+  then blk
+  else if intent = Out
+  then Term.prepend def_t blk def
+  else Term.append  def_t blk def
 
 let nullify_if xs ~f =
   List.map xs ~f:(fun x -> if f x then 0 else x)
@@ -81,35 +98,43 @@ let nullify_inputs ivars = nullify_if ~f:(Set.mem ivars)
 let nullify_outputs ivars =
   nullify_if ~f:(Fn.non (Set.mem ivars))
 
-let seed_jmp prog jmp cons ivars sub pat =
-  let seed_call intent inserter target id vs sub =
+(* here goes a small name clash, that can confuse - [out] arguments
+   define input variables (i.e., those variables that are bound to
+   values defined by a function), and [in] arguments define output
+   variables (i.e., those variables that are bound to values used by
+   a function) *)
+let seed_jmp prog jmp defn sub pat =
+  let seed_call intent target id vs sub =
     call_of_jmp jmp  >>= fun caller ->
     require (call_matches caller id) >>= fun () ->
     callee caller prog >>= fun callee ->
     target caller sub  >>| fun blk ->
     Term.enum arg_t callee |>
-    defs_of_args intent vs cons |>
-    Seq.map ~f:(tag_arg_def jmp caller) |>
-    Seq.fold ~init:blk ~f:inserter |>
+    defs_of_args intent vs defn |>
+    List.map ~f:(fun f -> f (Term.tid jmp)) |>
+    List.fold ~init:blk ~f:(add_def intent) |>
     Term.update blk_t sub in
-  let inputs id vs  =
+  let ivars = Defn.ivars defn in
+  let defs id vs  =
     let vs = nullify_outputs ivars vs in
-    seed_call Out prepend_def return id vs in
-  let outputs id vs =
+    seed_call Out return id vs in
+  let uses id vs =
     let vs = nullify_inputs ivars vs in
     let self _ _ = Program.parent jmp_t prog (Term.tid jmp) in
-    seed_call In append_def self id vs in
+    seed_call In self id vs in
   match pat with
   | Pat.Call (id,v,vs) ->
     let seed f s =
       Option.value ~default:sub (f id (v::vs) s) in
-    seed inputs sub |> seed outputs
+    seed defs sub |> seed uses
   | _ -> sub
 
-let seed_def def cons vars blk pat =
+let seed_def def defn blk pat =
+  let vars = Defn.ivars defn in
+  let cons = Defn.constrs defn in
   let hit v = Set.mem vars v  in
   let free = Def.free_vars def in
-  let pred v = sat_pred cons def v free in
+  let pred v = exists_sat_pred cons def v free in
   let def = match pat with
     | Pat.Move (v1,v2)
     | Pat.Load (v1,v2)
@@ -121,15 +146,14 @@ let seed_def def cons vars blk pat =
   | None -> blk
   | Some def -> Term.update def_t blk def
 
+
 let fold_patts spec ~init ~f =
   List.fold spec ~init ~f:(fun init defn ->
-      let vars = Defn.ivars defn in
-      let cons = Defn.constrs defn in
       Defn.rules defn |>
       List.fold ~init ~f:(fun init rule ->
           let rules = Rule.premises rule @
                       Rule.conclusions rule in
-          List.fold rules ~init ~f:(fun init rule -> f cons vars init rule)))
+          List.fold rules ~init ~f:(fun init rule -> f defn init rule)))
 
 let seed_sub (spec : spec) prog sub =
   let defns = Spec.defns spec in
