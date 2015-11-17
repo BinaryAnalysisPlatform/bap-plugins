40247bd0ae62fbfbc536e8ba90b67e485160ef69
Ivan Gotovchits <ivg@ieee.org>
2015-11-16 21:13:38 -0500

    odbx: corrected signature according to an gnueabi

--- header/odbx.h
+++ header/odbx.h
@@ -1,7 +1,7 @@
 // odbx.cpp
 
 // Stmt Conn:create(const char* sql, Stmt::Type type)
-void* _ZN7OpenDBX4Conn6createERKSsNS_4Stmt4TypeE(const char* sql, int type);
+void _ZN7OpenDBX4Conn6createERKSsNS_4Stmt4TypeE(void *this, void *stmt, const char* sql, int type);
 
 // TODO overloaded Conn:create:
 // Stmt Conn::create( const char* sql, unsigned long length, Stmt::Type type )
@@ -16,7 +16,7 @@ void* _ZN7OpenDBX4Conn6escapeERKSsRSs(const void* from, void* to);
 // string& Conn::escape( const char*, unsigned long fromlen, string& to)
 
 // string& std::string::append(std::string const&)
-void* _ZNSs6appendERKSs(const void* str);
+void _ZNSs6appendERKSs(void *this, const char *data);
 
 // string& std::string::append(char const*, unsigned int)
-void* _ZNSs6appendEPKcj(char const*, unsigned int);
+void _ZNSs6appendEPKcj(void *this, const char *data, unsigned int);
